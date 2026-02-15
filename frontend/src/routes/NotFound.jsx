1723352991469a153caafe2e8080b72b667fe7fe
Saflaski <sabeehislam.si@gmail.com>
2026-02-15 17:05:09 +0530

    404 page update

--- frontend/src/routes/NotFound.jsx
+++ frontend/src/routes/NotFound.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
-import '/src/assets/styles/auxPages.css'
+import "/src/assets/styles/auxPages.css";
 
 export function NotFound() {
-
-    const [fact, setFact] = useState("");
+  const [fact, setFact] = useState("");
 
   useEffect(() => {
     fetch("https://uselessfacts.jsph.pl/today.json?language=en")
@@ -12,27 +11,25 @@ export function NotFound() {
       .catch((err) => console.error("Error fetching fact:", err));
   }, []);
 
-    return (
-        <div className="text-center min-h-full">
-            <p className="text-gray-950 text-6xl">
-            Oops, this page does not seem to exist! 
-            </p>
-            <p className="text-gray-600 text-s mt-3">
-                Anyway, here's the random fact of the day: {fact || "Loading.."}
-            </p>
-            <footer className="text-sm text-gray-500 border-t mt-10 position-fixed align-bottom">
-                Facts provided by{" "}
-                <a
-                    href="https://uselessfacts.jsph.pl/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-500 hover:underline"
-                >
-                    uselessfacts.jsph.pl
-                </a>
-            </footer>
-        </div >
-            
-        
-    )
+  return (
+    <div className="text-center min-h-full">
+      <p className="text-gray-950 font-[Sora] mt-10 text-6xl">
+        Oops, this page does not seem to exist!
+      </p>
+      <p className="text-gray-600 text-s mt-3">
+        Anyway, here's the random fact of the day: {fact || "Loading.."}
+      </p>
+      <footer className="text-sm text-gray-500 border-t mt-10 position-fixed align-bottom">
+        Facts provided by{" "}
+        <a
+          href="https://uselessfacts.jsph.pl/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline"
+        >
+          uselessfacts.jsph.pl
+        </a>
+      </footer>
+    </div>
+  );
 }
